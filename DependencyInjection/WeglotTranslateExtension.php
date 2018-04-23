02ccc43a4f13888abf1f36be62a2a080348827db
Baptiste Leduc <baptiste@weglot.com>
2018-04-23 12:08:35 +0200

    adding utility function & imports

--- DependencyInjection/WeglotTranslateExtension.php
+++ DependencyInjection/WeglotTranslateExtension.php
@@ -5,8 +5,11 @@ namespace Weglot\TranslateBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\Kernel;
+use Weglot\Client\Client;
 
 /**
  * Class WeglotTranslateExtension
@@ -43,5 +46,15 @@ class WeglotTranslateExtension extends Extension
         // then load all other dependencies
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * @param string $subject
+     * @param string $needed
+     * @return bool
+     */
+    private function stringStartWith($subject, $needed)
+    {
+        return preg_match('#^' .$needed. '#i', $subject);
     }
 }
